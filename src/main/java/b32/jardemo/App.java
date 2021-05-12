c35ff5987c133ba163369eb0399d443b6ffa2c9e
ay735 <ranjanayush011@gmail.com>
2021-05-12 12:10:31 +0530

    v1

---
+++ src/main/java/b32/jardemo/App.java
@@ -0,0 +1,13 @@
+package b32.jardemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
